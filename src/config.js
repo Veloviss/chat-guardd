0f77d946575706f7b0c3e01a6ffd9209f1ff7aa1
Xvolor <75175684+Xvolor@users.noreply.github.com>
2021-08-12 15:12:26 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: 'ODc1MTE0OTIxMDQ4ODA1Mzc3.YRQ0SA.-AYquTzGWOHnv55BqLHcQqFalAo',
+    MongoDB_ConnectURL: 'mongodb+srv://Ateschat:zBAvb3jj2VupiYQM@ateschat.qmpqn.mongodb.net/ateschat?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '.',
+    BotOwners: ["372396594697469953"],
+    Custom_Status_Text: 'This is Velovis',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'dnd', // => dnd / idle / online / invisible
+    VoiceChannelID: '872812450980986970'
+};
+  
+module.exports = config;
---
+++ start.bat
@@ -0,0 +1,7 @@
+echo off
+title Chat Guard 2.0
+color d
+cls
+:a
+node velovis.js
+goto a
